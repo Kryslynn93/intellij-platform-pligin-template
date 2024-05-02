30ff6f05df432d455768788e5f7ce85b5460a515
GitHub Action <action@github.com>
2024-05-02 21:50:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/kryslynn93/intellijplatformpligintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/kryslynn93/intellijplatformpligintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kryslynn93.intellijplatformpligintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kryslynn93/intellijplatformpligintemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/kryslynn93/intellijplatformpligintemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kryslynn93.intellijplatformpligintemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
